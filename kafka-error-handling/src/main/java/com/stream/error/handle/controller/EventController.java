53cc69f43efef53eefdbe9be23c69bd8e79de107
Your Name <you@example.com>
2024-10-13 04:38:35 +0530

    program added

--- kafka-error-handling/src/main/java/com/stream/error/handle/controller/EventController.java
+++ kafka-error-handling/src/main/java/com/stream/error/handle/controller/EventController.java
@@ -1,8 +1,13 @@
 package com.stream.error.handle.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.stream.error.handle.dto.UserRequest;
 import com.stream.error.handle.util.CsvReaderUtils;
 import com.stream.error.handle.model.User;
 import com.stream.error.handle.producer.KafkaMessageProducer;
+
+import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,15 +23,27 @@ public class EventController {
     private KafkaMessageProducer producer;
 
     @Autowired
-    public EventController(KafkaMessageProducer publisher) {
-        this.producer = publisher;
+    public EventController(KafkaMessageProducer producer) {
+        this.producer = producer;
     }
 
-    @PostMapping("/publish")
-    public ResponseEntity<?> publishEvent() {
+    @PostMapping("/publish/user")
+    public ResponseEntity<?> publishUserEvent(@RequestBody UserRequest userRequest) {
+
+
         try {
-            List<User> users = CsvReaderUtils.readDataFromCsv();
-            users.forEach(usr -> producer.sendEvents(usr));
+            User user = User.builder().id(userRequest.getId()).email(userRequest.getEmail()).gender(userRequest.getGender()).firstName(userRequest.getFirstName()).lastName(userRequest.getLastName()).ipAddress(userRequest.getIpAddress()).build();
+            producer.sendUserEvent(user);
+            return ResponseEntity.ok("Message published successfully");
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @PostMapping("/publish/csv")
+    public ResponseEntity<?> publishEventFromCsvUser() {
+        try {
+
+            producer.publishCsvUserEvent();
             return ResponseEntity.ok("Message published successfully");
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
--- kafka-error-handling/src/main/java/com/stream/error/handle/producer/KafkaMessageProducer.java
+++ kafka-error-handling/src/main/java/com/stream/error/handle/producer/KafkaMessageProducer.java
@@ -1,13 +1,18 @@
 package com.stream.error.handle.producer;
 
 import com.stream.error.handle.model.User;
+import com.stream.error.handle.util.CsvReaderUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Service
@@ -25,13 +30,12 @@ public class KafkaMessageProducer {
 
 
 
-    public void sendEvents(User user) {
+    public void sendUserEvent(User user) {
         try {
             CompletableFuture<SendResult<String, User>> future = kafkaTemplate.send(topicName, user);
             future.whenComplete((result, ex) -> {
                 if (ex == null) {
-                    System.out.println("Sent message=[" + user.toString() +
-                            "] with offset=[" + result.getRecordMetadata().offset() + "]");
+                    //System.out.println("Sent message=[" + user.toString() + "] with offset=[" + result.getRecordMetadata().offset() + "]");
 
                     System.out.println("Sent record to topic " + result.getRecordMetadata().topic() +
                                " partition " + result.getRecordMetadata().partition() +
@@ -43,6 +47,16 @@ public class KafkaMessageProducer {
             });
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
+        }
+    }
+
+    public ResponseEntity<String> publishCsvUserEvent(){
+        try {
+            List<User> users = CsvReaderUtils.readDataFromCsv();
+            users.forEach(usr -> kafkaTemplate.send((Message<?>) usr));
+            return ResponseEntity.ok("Message published successfully");
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
